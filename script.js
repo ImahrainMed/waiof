12827580522f84f9e528370777ea66714d84dc5d
ImahrainMed <mohammedimahrain@hotmail.com>
2024-09-07 17:23:48 +0200

    intial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	$(".count").each(function () {
+              $(this)
+                .prop("Counter", 0)
+                .animate(
+                  {
+                    Counter: $(this).text(),
+                  },
+                  {
+                    duration: 4000,
+                    easing: "swing",
+                    step: function (now) {
+                      now = Number(Math.ceil(now)).toLocaleString('en');
+                                            $(this).text(now);
+                    },
+                  }
+                );
+        
+            });
+
+});
+
